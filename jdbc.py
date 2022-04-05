37c27f5d7e6f87c36569de9b265c610a58762d20
Alex Merced <alex@alexmerced.dev>
2022-04-05 16:44:11 -0400

    update

---
+++ jdbc.py
@@ -0,0 +1,29 @@
+## Import Time
+import time
+
+## Import pyodbc
+import jaydebeapi
+
+## get configurations from config.json
+from getConfig import getConfig
+config = getConfig()
+
+## Create JDBC Connection String
+connection_string=f"{config.get('JDBC_string', 'OOPS')}username=$token;password={config.get('personalKey', 'OOPS')};"
+
+print(connection_string)
+
+## Establish Connection
+conn = jaydebeapi.connect("com.dremio.jdbc.Driver",
+                          connection_string,{"ssl": "false"},config.get("jdbcjar", "oops"))
+
+## Create Cursor
+curs = conn.cursor()
+
+## Send Query
+curs.execute("SELECT * FROM \"@dremio.demo@gmail.com\".\"nyc-taxi-data\" limit 100")
+curs.fetchall()
+
+## Close Connection
+curs.close()
+conn.close()
