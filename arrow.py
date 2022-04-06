824f6ee83aef744de56304f552829645ecb086e9
Alex Merced <alex@alexmerced.dev>
2022-04-06 10:20:06 -0400

    update

--- arrow.py
+++ arrow.py
@@ -34,23 +34,35 @@ client = FlightClient(location=("grpc+tls://data.dremio.cloud:443"))
 ## makeQuery function
 def make_query(query, client, headers):
     
+    sbegin = time.time()
     ## Get Schema Description and build headers
     flight_desc = flight.FlightDescriptor.for_command(query)
     options = flight.FlightCallOptions(headers=headers)
     schema = client.get_schema(flight_desc, options)
+    send = time.time()
+    print(f"getting schema took {send-sbegin} seconds")
     
+    sbegin = time.time()
     ## Get ticket to for query execution, used to get results
     flight_info = client.get_flight_info(flight.FlightDescriptor.for_command(query), options)
+    send = time.time()
+    print(f"getting ticket took {send-sbegin} seconds")
     
+    sbegin = time.time()
     ## Get Results 
-    return client.do_get(flight_info.endpoints[0].ticket, options)
+    results = client.do_get(flight_info.endpoints[0].ticket, options)
+    send = time.time()
+    print(f"getting results took {send-sbegin} seconds")
+    return results
 
 #----------------------------------
 # Run Query
 #----------------------------------
 
-begin = time.time()
+begin = time.time() # Start Clock
 results = make_query("SELECT * FROM \"@dremio.demo@gmail.com\".\"nyc-taxi-data\" limit 100", client, headers)
-end = time.time()
+end = time.time() # End Clock
 
-print(f"Query Completed in {begin-end} seconds")
+
+print(results.read_pandas())
+print(f"Query Completed in {end-begin} seconds")
